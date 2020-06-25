fce63e86f73034c457c6bda45b305be521e7b403
Rafael Paranhos <rafaelparanhos@gmail.com>
2020-06-25 20:49:42 -0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    try:
+        with open('database.txt', mode='a') as database:
+            database.write(data['email'] + ' - ')
+            database.write(data['subject'] + ' - ')
+            database.write(data['message'] + '\n')
+    except FileNotFoundError:
+        print('check the directory')
+
+
+def write_to_csv(data):
+    try:
+        with open('database.csv', mode='a', newline='') as database2:
+            email = data['email']
+            subject = data['subject']
+            message = data['message']
+            csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([email, subject, message])
+    except FileNotFoundError:
+        print('check the directory')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong'
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+#
+#
+# # @app.route('/components.html')
+# # def components():
+# #     return render_template('components.html')
+#
+#
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+#
+#
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
